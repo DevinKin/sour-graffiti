2e6ed2b6eac5dd9ca3e765461626a80be456e33d
devin <devinkin@163.com>
2024-03-04 13:41:43 +0800

    feat: add minio component

---
+++ components/minio/test/sour/graffiti/minio/interface_test.clj
@@ -0,0 +1,6 @@
+(ns sour.graffiti.minio.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [sour.graffiti.minio.interface :as minio]))
+
+(deftest dummy-test
+  (is (= 1 1)))
